b48ba44bf7b7e65957710fb1351a761d80c59696
calzheng <calzheng@umich.edu>
2020-07-30 00:16:20 -0400

    Simplified Code and Netlify Pushed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import ToDo from "./components/Todo";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <ToDo/>
+      <Todo/>
     </div>
   );
 }
