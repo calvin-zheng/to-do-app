1b7ef3b880e38ec0d86af372f38bd255ef2303b1
calzheng <calzheng@umich.edu>
2020-08-09 22:52:04 -0400

    Rewrote Todo for class-based to allow for proper props passing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,145 +1,144 @@
-    import React, { useState, useEffect } from 'react';
-    import Editable from './Editable';
+import React, {Component} from 'react';
+import Editable from './Editable';
 
-    function Todo() {
-        const [currentTodo, setCurrentTodo] = useState("");
-        const [todos, setTodos] = useState([]);
-        const [nComplete, setComplete] = useState(0);
-        const [category, setCategory] = useState("");
-        const [categories, setCategories] = useState("");
+class Todo extends Component {
+    constructor() {
+        super();
 
-        useEffect(() => {
-            const list = localStorage.getItem('todo');
-            const todoItems = JSON.parse(list);
-            setTodos(todoItems);
-            if(JSON.parse(localStorage.getItem('nComplete')) != null) {
-                setComplete(JSON.parse(localStorage.getItem('nComplete')));
-            }
-        }, []);
-
-        function addTaskCompleted(){
-            let new_complete = nComplete + 1;
-            setComplete(new_complete);
-            localStorage.setItem('nComplete', JSON.stringify(new_complete));
-        }
-
-        function removeTaskCompleted(){
-            let new_complete = nComplete - 1;
-            setComplete(new_complete);
-            localStorage.setItem('nComplete', JSON.stringify(new_complete));
-        }
-
-        function createNewTodo(currentTodo) {
-            if(todos != null) {
-                let todosArray = [...todos];
-                todosArray.push({
-                    todo: currentTodo,
-                    isCompleted: false,
-                    category: ""
-                });
-                setTodos(todosArray);
-                const todoData = JSON.stringify(todosArray);
-                localStorage.setItem('todo', todoData);
-            }
-            else{
-                let firstTodo = [{
-                    todo: currentTodo,
-                    isCompleted: false,
-                    category: ""
-                }];
-                setTodos(firstTodo);
-                const todoData = JSON.stringify(firstTodo);
-                localStorage.setItem('todo', todoData);
-            }
-
-
-        }
-
-        function completeTodo(index) {
-            let todosArray = [...todos];
-            todosArray[index].isCompleted = !todosArray[index].isCompleted;
-            if(todosArray[index].isCompleted){
-                addTaskCompleted()
-            }
-            else{
-                removeTaskCompleted()
-            }
-            setTodos(todosArray);
-            const todoData = JSON.stringify(todosArray);
-            localStorage.setItem('todo', todoData);
-        }
-
-        function deleteTodo(index) {
-            let todosArray = [...todos];
-            todosArray.splice(index, 1);
-            setTodos(todosArray);
-            const todoData = JSON.stringify(todosArray);
-            localStorage.setItem('todo', todoData);
-        }
-
-        function addCategory(index, category){
-            let todosArray = [...todos];
-            todosArray[index].category = category;
-            setTodos(todosArray);
-            const todoData = JSON.stringify(todosArray);
-            localStorage.setItem('todo', todoData);
-        }
-
-
-
-    return (
-        <div>
-        <input className="todo-input"
-               value={currentTodo}
-               onChange={e => {
-                   setCurrentTodo(e.target.value);
-               }}
-               onKeyPress={e => {
-                   if (e.key === "Enter") {
-                       createNewTodo(currentTodo);
-                       setCurrentTodo("");
-                   }
-               }}
-               placeholder="What needs to get done?"
-        />
-        <div className="list-of-todos">
-            {
-                todos  && todos.map((todo, index) => (
-                    <div key={todo} className="todo">
-                        <div className="checkbox" onClick={() => completeTodo(index)}>
-                            {todo.isCompleted && <span>&#x2714;</span>}
-                        </div>
-                        <div className={todo.isCompleted ? "done" : ""}>{todo.todo}</div>
-                        <div className = "category">
-                            <Editable
-                                text={todo.category}
-                                placeholder={"Add Category"}
-                                type="input"
-                                >
-                                    <input
-                                        type="text"
-                                        name="category"
-                                        placeholder="Category"
-                                        value={todo.category}
-                                        onChange={e => addCategory(index, e.target.value)
-                                            }
-                                        />
-                            </Editable>
-                        </div>
-                        <div className="delete" onClick={() => deleteTodo(index)}>
-                            &#128465;
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
-            <div className = "completed" >
-                {`${nComplete} items completed`}
-            </div>
-        </div>
-    );
+        this.state = {
+            currentTodo: "",
+            todos: [],
+            nComplete: 0
+        };
     }
 
 
+    addTaskCompleted(){
+        let new_complete = this.nComplete + 1;
+        this.setState({nComplete: new_complete});
+        localStorage.setItem('nComplete', JSON.stringify(new_complete));
+    }
 
-    export default Todo;
+    removeTaskCompleted(){
+        let new_complete = this.nComplete - 1;
+        this.setState({nComplete: new_complete});
+        localStorage.setItem('nComplete', JSON.stringify(new_complete));
+    }
+
+    createNewTodo(currentTodo) {
+        if(this.state.todos != null) {
+            let todosArray = [...this.state.todos];
+            todosArray.push({
+                todo: currentTodo,
+                isCompleted: false,
+                category: ""
+            });
+            this.setState({todos: todosArray});
+            const todoData = JSON.stringify(todosArray);
+            localStorage.setItem('todo', todoData);
+        }
+        else{
+            let firstTodo = [{
+                todo: currentTodo,
+                isCompleted: false,
+                category: ""
+            }];
+            this.setState({todos: firstTodo});
+            const todoData = JSON.stringify(firstTodo);
+            localStorage.setItem('todo', todoData);
+        }
+
+
+    }
+
+    completeTodo(index) {
+        let todosArray = [...this.state.todos];
+        todosArray[index].isCompleted = !todosArray[index].isCompleted;
+        if(todosArray[index].isCompleted){
+            this.addTaskCompleted();
+        }
+        else{
+            this.removeTaskCompleted();
+        }
+        this.setState({todos: todosArray});
+        const todoData = JSON.stringify(todosArray);
+        localStorage.setItem('todo', todoData);
+    }
+
+    deleteTodo(index) {
+        let todosArray = [...this.state.todos];
+        todosArray.splice(index, 1);
+        this.setState({todos: todosArray});
+        const todoData = JSON.stringify(todosArray);
+        localStorage.setItem('todo', todoData);
+    }
+
+    addCategory(index, category){
+        let todosArray = [...this.state.todos];
+        todosArray[index].category = category;
+        this.setState({todos: todosArray});
+        const todoData = JSON.stringify(todosArray);
+        localStorage.setItem('todo', todoData);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input className="todo-input"
+                       value={this.state.currentTodo}
+                       onChange={e => {
+                           this.setState({currentTodo: e.target.value});
+                       }}
+                       onKeyPress={e => {
+                           if (e.key === "Enter") {
+                               this.createNewTodo(this.state.currentTodo);
+                               this.setState({currentTodo: ""});
+                           }
+                       }}
+                       placeholder="What needs to get done?"
+                />
+                <div className="list-of-todos">
+                    {
+                        this.state.todos && this.state.todos.map((todo, index) => (
+                            <div key={todo} className="todo">
+                                <div className="checkbox" onClick={() => this.completeTodo(index)}>
+                                    {todo.isCompleted && <span>&#x2714;</span>}
+                                </div>
+                                <div className={todo.isCompleted ? "done" : ""}>{todo.todo}</div>
+                                <div className="category">
+                                    <Editable
+                                        text={todo.category}
+                                        placeholder={"Add Category"}
+                                        type="input"
+                                    >
+                                        <input
+                                            type="text"
+                                            name="category"
+                                            placeholder="Category"
+                                            value={todo.category}
+                                            onChange={e => this.addCategory(index, e.target.value)
+                                            }
+                                        />
+                                    </Editable>
+                                </div>
+                                <div className="delete" onClick={() => this.deleteTodo(index)}>
+                                    &#128465;
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="completed">
+                    {`${this.state.nComplete} items completed`}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default Todo;
+
+
